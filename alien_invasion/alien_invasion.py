5ddd37bb7808bfb3e684441ffd50cf46f07dc9bc
totoro <892692504@qq.com>
2020-07-21 23:19:07 +0800

    refactor run_game

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,5 +1,6 @@
-import sys
 import pygame
+
+import game_functions as gf
 from settings import Settings
 from ship import Ship
 
@@ -16,13 +17,8 @@ def run_game():
     ship = Ship(screen)
     # 开始游戏的主循环
     while True:
-        screen.fill(ai_settings.bg_color)  # 设置背景色
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        # 让最近绘制的屏幕可见
-        ship.blitme()
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
