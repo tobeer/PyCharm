8eb9a1494bad0b2d6588f1d68cd6311168ab6056
totoro <892692504@qq.com>
2020-08-04 23:07:00 +0800

    update alien-invasion

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -13,9 +13,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_DOWN:
         ship.rect.bottom += 1
     elif event.key == pygame.K_SPACE:
-        # 创建一颗子弹，并将其加入编组bullets中
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 
 def check_keyup_events(event, ship):
@@ -45,3 +43,21 @@ def update_screen(ai_settings, screen, ship, bullets):
     ship.blitme()
     # 让最近绘制的屏幕可见
     pygame.display.flip()
+
+
+def update_bullets(bullets):
+    # 更新子弹的位置
+    bullets.update()
+
+    # 删除已消失的子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """如果还没到达限制，就发射一颗子弹"""
+    # 创建一颗子弹，并将其加入编组bullets中
+    if len(bullets) < ai_settings.bullet_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -11,3 +11,4 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 10
         self.bullet_color = 60, 60, 60
+        self.bullet_allowed = 3
